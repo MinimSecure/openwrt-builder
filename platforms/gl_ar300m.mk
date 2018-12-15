3b32d842ad6171f75c993c55670a1815c36a0830
Tyler Sommer <sommertm@gmail.com>
2018-12-15 11:00:23 -0700

    Add files/configs for remaining supported platforms

---
+++ platforms/gl_ar300m.mk
@@ -0,0 +1,4 @@
+PLATFORM_NICE_NAME := GL.iNet AR-300M
+PLATFORM_NAME := gl_ar300m
+PLATFORM_CHIP := ar71xx
+PLATFORM_SDK  := https://downloads.openwrt.org/releases/18.06.1/targets/ar71xx/generic/openwrt-sdk-18.06.1-ar71xx-generic_gcc-7.3.0_musl.Linux-x86_64.tar.xz
---
+++ platforms/linksys-wrt1900acs.mk
@@ -0,0 +1,4 @@
+PLATFORM_NICE_NAME := Linksys WRT1900ACS
+PLATFORM_NAME := linksys-wrt1900acs
+PLATFORM_CHIP := mvebu-cortexa9
+PLATFORM_SDK  := https://downloads.openwrt.org/releases/18.06.1/targets/mvebu/cortexa9/openwrt-sdk-18.06.1-mvebu-cortexa9_gcc-7.3.0_musl_eabi.Linux-x86_64.tar.xz
