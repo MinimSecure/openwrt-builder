22f13f7b9dab7b68e693f3aa5c9783c72fb796bf
Tyler Sommer <sommertm@gmail.com>
2018-12-15 11:04:34 -0700

    Fix flip-flopped v2/v4 in archer_c7 makefiles

--- platforms/archer_c7_v2.mk
+++ platforms/archer_c7_v2.mk
@@ -1,4 +1,4 @@
-PLATFORM_NICE_NAME := TP-LINK Archer C7 v4
-PLATFORM_NAME := archer_c7_v4
+PLATFORM_NICE_NAME := TP-LINK Archer C7 v2
+PLATFORM_NAME := archer_c7_v2
 PLATFORM_CHIP := ar71xx
 PLATFORM_SDK  := https://downloads.openwrt.org/releases/18.06.1/targets/ar71xx/generic/openwrt-sdk-18.06.1-ar71xx-generic_gcc-7.3.0_musl.Linux-x86_64.tar.xz
